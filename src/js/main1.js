5c09c03e3f383a3202558a6b1e0685c849d0e266
AndresMora20201025123 <juamorap@udistrital.edu.co>
2023-12-02 14:31:03 -0500

    Capas

--- src/js/main1.js
+++ src/js/main1.js
@@ -5,6 +5,32 @@ import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 
+//Capa GeoJSON
+const geojsonLayer = new VectorLayer ({
+  source: new VectorSource ({
+      url: '/datos/Estaciones_Troncales_de_TRANSMILENIO.geojson', //Ruta de archivo GeoJSON
+      format: new GeoJSON ()
+  })
+});
+const geojsonLayer01 = new VectorLayer ({
+  source: new VectorSource ({
+      url: '/datos/Rutas_Troncales_de_TRANSMILENIO.geojson', //Ruta de archivo GeoJSON
+      format: new GeoJSON ()
+  })
+});
+const geojsonLayer02 = new VectorLayer ({
+  source: new VectorSource ({
+      url: '/datos/malla-vial37.geojson', //Ruta de archivo GeoJSON
+      format: new GeoJSON ()
+  })
+});
+const geojsonLayer03 = new VectorLayer ({
+  source: new VectorSource ({
+      url: '/datos/barrios-bogota.geojson', //Ruta de archivo GeoJSON
+      format: new GeoJSON ()
+  })
+});
+
 // Crear una instancia del mapa
 var map = new Map({
   target: 'map', // ID del elemento HTML donde se mostrará el mapa
@@ -18,3 +44,9 @@ var map = new Map({
     zoom: 10 // Nivel de zoom inicial
   })
 });
+
+
+map.addLayer (geojsonLayer);
+map.addLayer (geojsonLayer01);
+map.addLayer (geojsonLayer02);
+map.addLayer (geojsonLayer03);
