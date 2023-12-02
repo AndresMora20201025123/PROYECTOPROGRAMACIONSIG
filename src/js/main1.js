d064154272a5c65287e925e63ae633ecab2bad8d
AndresMora20201025123 <juamorap@udistrital.edu.co>
2023-12-02 13:58:51 -0500

    Mapa base

--- src/js/main1.js
+++ src/js/main1.js
@@ -1,13 +1,20 @@
 // main1.js
 //import 'ol/ol.css';
-import { Map, View } from 'ol';
+import  Map from 'ol/Map';
+import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 
-document.addEventListener('DOMContentLoaded', function() {
-  const map = new Map({
-    target: 'map',
-    layers: [new TileLayer({ source: new OSM() })],
-    view: new View({ center: [0, 0], zoom: 2 })
-  });
+// Crear una instancia del mapa
+var map = new Map({
+  target: 'map', // ID del elemento HTML donde se mostrará el mapa
+  layers: [
+    new TileLayer({
+      source: new OSM() // Capa base de OpenStreetMap
+    })
+  ],
+  view: new View({
+    center: [0, 0], // Coordenadas del centro del mapa
+    zoom: 10 // Nivel de zoom inicial
+  })
 });
