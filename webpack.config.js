16a5e893c566363e58db915f539d3facff2796cf
AndresMora20201025123 <juamorap@udistrital.edu.co>
2023-12-02 16:12:32 -0500

    Ajustes

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main1: './src/js/main1.js',
-    main2: './src/js/main2.js',
-    main3: './src/js/main3.js'
+    main1: './src/js/main1.js'
   },
   output: {
     filename: '[name].bundle.js',
